c280f5f4526eab9323c619a34ab34eb9b5067c7e
Ritwik Vaidya <vaidyaritwik@gmail.com>
2023-04-30 21:32:54 -0400

    add double check locking

--- Singleton/logger.cpp
+++ Singleton/logger.cpp
@@ -16,10 +16,12 @@ void Logger::Log(string message) {
 
 Logger* Logger::getInstance() {
 
-    mtx.lock();
     if (loggerInstance == nullptr) {
-        loggerInstance = new Logger();
+        mtx.lock();
+        if (loggerInstance == nullptr) {
+            loggerInstance = new Logger();
+        }
+        mtx.unlock();
     }
-    mtx.unlock();
     return loggerInstance;
 }
